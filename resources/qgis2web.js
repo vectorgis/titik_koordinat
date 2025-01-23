b4568b9c6fd02221114acef82cc79aef9185b7e6
biboae <bibobrobro@gmail.com>
2025-01-23 21:10:03 +0700

    update

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -150,9 +150,9 @@ function createPopupField(currentFeature, currentFeatureKeys, layer) {
 			} else {
 				var fieldValue = currentFeature.get(currentFeatureKeys[i]);
 				if (/\.(gif|jpg|jpeg|tif|tiff|png|avif|webp|svg)$/i.test(fieldValue)) {
-					popupField += (fieldValue != null ? '<img src="images/' + fieldValue.replace(/[\\\/:]/g, '_').trim() + '" /></td>' : '');
+					popupField += (fieldValue != null ? '<img src="titik_koordinat/images/' + fieldValue.replace(/[\\\/:]/g, '_').trim() + '" /></td>' : '');
 				} else if (/\.(mp4|webm|ogg|avi|mov|flv)$/i.test(fieldValue)) {
-					popupField += (fieldValue != null ? '<video controls><source src="images/' + fieldValue.replace(/[\\\/:]/g, '_').trim() + '" type="video/mp4">Il tuo browser non supporta il tag video.</video></td>' : '');
+					popupField += (fieldValue != null ? '<video controls><source src="titik_koordinat/images/' + fieldValue.replace(/[\\\/:]/g, '_').trim() + '" type="video/mp4">Il tuo browser non supporta il tag video.</video></td>' : '');
 				} else {
 					popupField += (fieldValue != null ? autolinker.link(fieldValue.toLocaleString()) + '</td>' : '');
 				}
